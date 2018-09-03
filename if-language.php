e278fd290c10e9fd3a41f44ac54583f3575830ec
0ellin <41482913+0ellin@users.noreply.github.com>
2018-09-03 14:56:37 +0200

    Create if-language.php

---
+++ if-language.php
@@ -0,0 +1,5 @@
+ <?php if($currentLan == 'en'){ ?>
+<!-----------CODE HERE IF CONDITION IS OK -------------!>
+ <?php }else{ ?>
+<!-----------CODE HERE IF CONDITION IS NOT OK -------------!>
+ <?php } ?>
