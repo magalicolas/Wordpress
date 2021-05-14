14eac7fd735a4872dcbbe7e3da8d7631a050bade
magalicolas <41482913+magalicolas@users.noreply.github.com>
2021-05-14 11:28:23 +0200

    Create stop-spam-gravity-form.php

---
+++ stop-spam-gravity-form.php
@@ -0,0 +1,6 @@
+new GWEmailDomainControl(array(
+'form_id' => 152, //id of your form
+'field_id' => 9, // id fiel
+'domains' => array('yopmail.com', 'test.com', 'test.fr'),
+'mode' => 'limit' // 'limit' or 'ban'
+));
